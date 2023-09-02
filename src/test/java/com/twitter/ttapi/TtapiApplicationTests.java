780bfbe01c6ca7967b238a328ac7c42e8007fa1a
MarceloMosl <marcelomosl@icloud.com>
2023-09-02 17:18:56 -0300

    first commit

---
+++ src/test/java/com/twitter/ttapi/TtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.ttapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
